fe41583169578150c94502befad6989cbc275aae
Wendi Attaway Morris <wendiwebgirl@gmail.com>
2018-09-17 11:55:41 -0400

    Initial Commit

---
+++ GitExamplex9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExamplex9
+//
+//  Created by Wendi Attaway Morris on 9/17/18.
+//  Copyright © 2018 Advisors Academy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
